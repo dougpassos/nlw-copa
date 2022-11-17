3750e128d22d46dd73e3ef7ee222c0e2194555fa
Douglas Passos <douglas.santos.qa@gmail.com>
2022-11-17 10:41:07 -0300

    Add files via upload

---
+++ nlw/main.js
@@ -0,0 +1,351 @@
+const jsonCalendar = {
+    "20/11": {
+        "week": "domingo",
+        "games": [
+            {
+                "time1": "qatar",
+                "time2": "ecuador",
+                "hour": "13:00"
+            }
+        ]
+    },
+    "21/11": {
+        "week": "segunda",
+        "games": [
+            {
+                "time1": "england",
+                "time2": "iran",
+                "hour": "10:00"
+            },
+            {
+                "time1": "senegal",
+                "time2": "netherlands",
+                "hour": "13:00"
+            },
+            {
+                "time1": "usa",
+                "time2": "wales",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "22/11": {
+        "week": "terça",
+        "games": [
+            {
+                "time1": "argentina",
+                "time2": "saudi-arabia",
+                "hour": "07:00"
+            },
+            {
+                "time1": "denmark",
+                "time2": "tunisia",
+                "hour": "10:00"
+            },
+            {
+                "time1": "mexico",
+                "time2": "poland",
+                "hour": "13:00"
+            },
+            {
+                "time1": "france",
+                "time2": "australia",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "23/11": {
+        "week": "quarta",
+        "games": [
+            {
+                "time1": "marocco",
+                "time2": "croatia",
+                "hour": "07:00"
+            },
+            {
+                "time1": "germany",
+                "time2": "japan",
+                "hour": "10:00"
+            },
+            {
+                "time1": "spain",
+                "time2": "costa-rica",
+                "hour": "13:00"
+            },
+            {
+                "time1": "belgium",
+                "time2": "canada",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "24/11": {
+        "week": "quinta",
+        "games": [
+            {
+                "time1": "switzerland",
+                "time2": "cameroon",
+                "hour": "07:00"
+            },
+            {
+                "time1": "uruguay",
+                "time2": "south-korea",
+                "hour": "10:00"
+            },
+            {
+                "time1": "portugal",
+                "time2": "ghana",
+                "hour": "13:00"
+            },
+            {
+                "time1": "brazil",
+                "time2": "serbia",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "25/11": {
+        "week": "sexta",
+        "games": [
+            {
+                "time1": "wales",
+                "time2": "iran",
+                "hour": "07:00"
+            },
+            {
+                "time1": "qatar",
+                "time2": "senegal",
+                "hour": "10:00"
+            },
+            {
+                "time1": "netherlands",
+                "time2": "ecuador",
+                "hour": "13:00"
+            },
+            {
+                "time1": "england",
+                "time2": "usa",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "26/11": {
+        "week": "sabado",
+        "games": [
+            {
+                "time1": "tunisia",
+                "time2": "australia",
+                "hour": "07:00"
+            },
+            {
+                "time1": "poland",
+                "time2": "saudi-arabia",
+                "hour": "10:00"
+            },
+            {
+                "time1": "france",
+                "time2": "denmark",
+                "hour": "13:00"
+            },
+            {
+                "time1": "argentina",
+                "time2": "mexico",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "27/11": {
+        "week": "domingo",
+        "games": [
+            {
+                "time1": "japan",
+                "time2": "costa-rica",
+                "hour": "07:00"
+            },
+            {
+                "time1": "belgium",
+                "time2": "marocco",
+                "hour": "10:00"
+            },
+            {
+                "time1": "croatia",
+                "time2": "canada",
+                "hour": "13:00"
+            },
+            {
+                "time1": "spain",
+                "time2": "germany",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "28/11": {
+        "week": "segunda",
+        "games": [
+            {
+                "time1": "cameroon",
+                "time2": "serbia",
+                "hour": "07:00"
+            },
+            {
+                "time1": "south-korea",
+                "time2": "ghana",
+                "hour": "10:00"
+            },
+            {
+                "time1": "brazil",
+                "time2": "switzerland",
+                "hour": "13:00"
+            },
+            {
+                "time1": "portugal",
+                "time2": "uruguay",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "29/11": {
+        "week": "terça",
+        "games": [
+            {
+                "time1": "ecuador",
+                "time2": "senegal",
+                "hour": "12:00"
+            },
+            {
+                "time1": "netherlands",
+                "time2": "qatar",
+                "hour": "12:00"
+            },
+            {
+                "time1": "iran",
+                "time2": "usa",
+                "hour": "16:00"
+            },
+            {
+                "time1": "wales",
+                "time2": "england",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "30/11": {
+        "week": "quarta",
+        "games": [
+            {
+                "time1": "tunisia",
+                "time2": "france",
+                "hour": "12:00"
+            },
+            {
+                "time1": "australia",
+                "time2": "denmark",
+                "hour": "12:00"
+            },
+            {
+                "time1": "poland",
+                "time2": "argentina",
+                "hour": "16:00"
+            },
+            {
+                "time1": "saudi-arabia",
+                "time2": "mexico",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "01/12": {
+        "week": "quinta",
+        "games": [
+            {
+                "time1": "croatia",
+                "time2": "belgium",
+                "hour": "12:00"
+            },
+            {
+                "time1": "canada",
+                "time2": "marocco",
+                "hour": "12:00"
+            },
+            {
+                "time1": "japan",
+                "time2": "spain",
+                "hour": "16:00"
+            },
+            {
+                "time1": "costa-rica",
+                "time2": "germany",
+                "hour": "16:00"
+            }
+        ]
+    },
+    "02/12": {
+        "week": "sexta",
+        "games": [
+            {
+                "time1": "south-korea",
+                "time2": "portugal",
+                "hour": "12:00"
+            },
+            {
+                "time1": "ghana",
+                "time2": "uruguay",
+                "hour": "12:00"
+            },
+            {
+                "time1": "serbia",
+                "time2": "switzerland",
+                "hour": "16:00"
+            },
+            {
+                "time1": "cameroon",
+                "time2": "brazil",
+                "hour": "16:00"
+            }
+        ]
+    }
+}
+
+function createGame(player1, hour, player2) {
+    return `
+    <li>
+    <img src='./assets/countries/icon-${player1}.svg' alt="Bandeira do ${player1}">
+        <strong>${hour}</strong>
+        <img src='./assets/countries/icon-${player2}.svg' alt="Bandeira do ${player2}">
+        </li>`
+}
+    
+let delayCardAnimation = -0.3;
+
+function createCard(date, day, games) {
+    delayCardAnimation = delayCardAnimation + 0.3;
+    return `
+    <div class="card" style=animation-delay:${delayCardAnimation}s>
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+    ${games}                                  
+    </ul>
+    </div>`
+}    
+
+function createCalendar(calendar) {
+    let resultCard = "";
+    let resultGames = "";
+    for (const data in calendar) {
+        const games = calendar[data]["games"]
+        for (const game in games) {
+            resultGames += createGame(
+                                calendar[data]["games"][game]["time1"],
+                                calendar[data]["games"][game]["hour"],
+                                calendar[data]["games"][game]["time2"]
+                                );
+        }
+        resultCard += createCard(data, calendar[data]["week"], resultGames);
+        resultGames = ""      
+    }
+
+    return resultCard
+}
+
+document.querySelector('#cards').
+        innerHTML = createCalendar(jsonCalendar);
